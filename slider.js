5e8c342fd0ee65798266fa57fa0c2b2dfdb0440d
Egor Gromyko <egorgromyko@gmail.com>
2018-12-12 11:28:49 +0100

    resizing fix

--- slider.js
+++ slider.js
@@ -8,8 +8,11 @@ const createSlider = () => {
 		if (slides[slideIndex]) {
 			currentSlide = slideIndex;
 			slides[slideIndex].scrollIntoView();
+			log(slideIndex)
 		}
 	}
+
+	goTo(0);
 
 	return {
 		next() {
@@ -20,6 +23,9 @@ const createSlider = () => {
 		},
 		to(i) {
 			goTo(i);
+		},
+		current() {
+			return currentSlide;
 		}
 	}
 }
@@ -28,15 +34,20 @@ slider = createSlider();
 
 // interface
 onkeydown = (e) => { 
-	if (e.key === 'ArrowRight') {
+	if (e.key === 'ArrowRight' || e.key === 'ArrowDown') {
 		slider.next();
 	}
 
-	if (e.key === 'ArrowLeft') {
+	if (e.key === 'ArrowLeft' || e.key === 'ArrowUp') {
 		slider.previous();
 	}
 
 	if (e.key.match(/[0-9]/)) {
 		slider.to(e.key)
 	}
+}
+
+// fix resizing issues
+onresize = (e) => {
+	slider.to(slider.current())
 }
